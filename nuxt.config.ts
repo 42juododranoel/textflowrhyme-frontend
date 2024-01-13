0f7ee8545fe184596989dd8682e7ec40c80a39cd
Vsevolod Skripnik <vskripnik@bumerang.com>
2024-01-13 20:41:37 +0400

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,55 +1,64 @@
 export default defineNuxtConfig({
-  pages: true,
+	pages: true,
 
-  modules: [
-    "@pinia/nuxt",
-    [
-      "@nuxtjs/google-fonts",
-      {
-        families: {
-          "Fira Code": true,
-          "PT Sans": true,
-          "PT Serif": true,
-          "PT Sans Caption": {
-            wght: [700],
-          },
-        },
-      },
-    ],
-    "nuxt-proxy",
-  ],
+	modules: [
+		'@pinia/nuxt',
+		[
+			'@nuxtjs/google-fonts',
+			{
+				families: {
+					'Fira Code': true,
+					'PT Sans': true,
+					'PT Serif': true,
+					'PT Sans Caption': {
+						wght: [700],
+					},
+				},
+			},
+		],
+		'nuxt-proxy',
+	],
 
-  app: {
-    head: {
-      charset: "utf-8",
-      viewport: "width=device-width, initial-scale=1",
-      link: [
-        {
-          rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css",
-        },
-        {
-          rel: "stylesheet",
-          href: "https://cdn.jsdelivr.net/gh/mdbassit/Coloris@latest/dist/coloris.min.css",
-        },
-      ],
-      script: [
-        { src: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" },
-        { src: "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" },
-        { src: "https://cdn.jsdelivr.net/gh/mdbassit/Coloris@latest/dist/coloris.min.js" },
-      ],
-    },
-  },
+	app: {
+		head: {
+			charset: 'utf8',
+			viewport: 'width=device-width, initial-scale=1',
+			link: [
+				{
+					rel: 'stylesheet',
+					href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css',
+				},
+				{
+					rel: 'stylesheet',
+					href: 'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css',
+				},
+				{
+					rel: 'stylesheet',
+					href: 'https://cdn.jsdelivr.net/gh/mdbassit/Coloris@latest/dist/coloris.min.css',
+				},
+			],
+			script: [
+				{
+					src: 'https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js',
+				},
+				{
+					src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js',
+				},
+				{
+					src: 'https://cdn.jsdelivr.net/gh/mdbassit/Coloris@latest/dist/coloris.min.js',
+				},
+			],
+		},
+	},
 
-  proxy: {
-    options: {
-      target: "http://127.0.0.1:8000/",
-      changeOrigin: true,
-      pathFilter: ["/api/v1.0.0/documents/analyze", "/api/v1.0.0/texts/rewrite"],
-    },
-  },
-})
+	proxy: {
+		options: {
+			target: 'http://127.0.0.1:8000/',
+			changeOrigin: true,
+			pathFilter: [
+				'/api/v1.0.0/documents/analyze',
+				'/api/v1.0.0/texts/rewrite',
+			],
+		},
+	},
+});
--- utils/editor/nodes.js
+++ utils/editor/nodes.js
@@ -1,47 +1,59 @@
-import Paragraph from "@tiptap/extension-paragraph"
-import Document from "@tiptap/extension-document"
-import Text from "@tiptap/extension-text"
-import { mergeAttributes, Mark } from "@tiptap/core"
+import Paragraph from '@tiptap/extension-paragraph';
+import Document from '@tiptap/extension-document';
+import Text from '@tiptap/extension-text';
+import {mergeAttributes, Mark} from '@tiptap/core';
 
 export const FatigueMark = Mark.create({
-  name: "fatigue",
-  renderHTML({ HTMLAttributes }) {
-    return ["span", { class: `editor--fatigue__value${HTMLAttributes.value}` }, 0]
-  },
-  addAttributes() {
-    return {
-      value: {
-        default: 0,
-      },
-    }
-  },
-})
+	name: 'fatigue',
+	renderHTML({HTMLAttributes}) {
+		return [
+			'span',
+			{class: `editor--fatigue__value${HTMLAttributes.value}`},
+			0,
+		];
+	},
+	addAttributes() {
+		return {
+			value: {
+				default: 0,
+			},
+		};
+	},
+});
 
 export const LengthMark = Mark.create({
-  name: "length",
-  renderHTML({ HTMLAttributes }) {
-    return ["span", { class: `editor--sentence__length-${HTMLAttributes.value}` }, 0]
-  },
-  addAttributes() {
-    return {
-      value: {
-        default: 0,
-      },
-    }
-  },
-})
+	name: 'length',
+	renderHTML({HTMLAttributes}) {
+		return [
+			'span',
+			{class: `editor--sentence__length-${HTMLAttributes.value}`},
+			0,
+		];
+	},
+	addAttributes() {
+		return {
+			value: {
+				default: 0,
+			},
+		};
+	},
+});
 
 export const EditorText = Text.extend({
-  marks: "fatigue length",
-})
+	marks: 'fatigue length',
+});
 
 export const EditorParagraph = Paragraph.extend({
-  content: "text*",
-  renderHTML({ HTMLAttributes }) {
-    return ["p", mergeAttributes(HTMLAttributes, { class: "editor--paragraph" }), 0]
-  },
-})
+	content: 'text*',
+	renderHTML({HTMLAttributes}) {
+		return [
+			'p',
+			mergeAttributes(HTMLAttributes, {class: 'editor--paragraph'}),
+			0,
+		];
+	},
+});
 
 export const EditorDocument = Document.extend({
-  content: "paragraph+",
-})
+	content: 'paragraph+',
+});
